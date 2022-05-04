8b5e00162600901193ee34d6b5cbdafaf7a09058
YanXuan1 <xuan.yan@northeastern.edu>
2022-05-04 08:54:37 +0800

    small changes

--- my-app/src/frontend/components/FieldBuilder.js
+++ my-app/src/frontend/components/FieldBuilder.js
@@ -45,40 +45,37 @@ function FieldBuilder(props){
             });
         }
         return (
-            <div> 
-               
-            <form>
-            <label>Label</label> 
-           <input id="label" defaultValue={data.label}></input><br/>
-            <label>Type</label>
-            <label>Multi-select</label> 
-            <label><input type="checkbox" name="multi-select" value="true" defaultChecked={data.required}/>A Value is required</label><br/>
-            <label>DefaultValue</label> 
-           <input id="default" name="defaultValue" type="text" value = {data.defaultValue} onChange={stateChangeHandler}></input><br/>
-           <label>Choices</label> <br/> 
-           <table className='myTable'>
-               <tbody id='myTableBody'>
-                {
-                    choice.map((item,key)=>{
-                        return(
-                           <Items item={item} inp={inputChangeHanlder} remove={removeHanlder} key={key}></Items>
-                        )
-                    })
-                }
-                </tbody>
-                </table>
-               <br/>
-           <label>Order
-               <select>
-                   <option value="A">A</option> 
-                   <option value="B">B</option>
-                   <option value="C">C</option>
-               </select>
-           </label> 
-           <br/>
-             <button type="button" >Save</button> 
-             <button type="button" >concel</button> 
-            </form> 
+            <div>
+                <form>
+                    <label>Label</label>
+                    <input id="label" defaultValue={data.label}></input><br/>
+                    <label>Type</label>
+                    <label>Multi-select</label> 
+                    <label><input type="checkbox" name="multi-select" value="true" defaultChecked={data.required}/>A Value is required</label><br/>
+                    <label>DefaultValue</label> 
+                    <input id="default" name="defaultValue" type="text" value = {data.defaultValue} onChange={stateChangeHandler}></input><br/>
+                    <label>Choices</label><br/> 
+                    <table className='myTable'>
+                        <tbody id='myTableBody'>
+                            {
+                               choice.map((item,key)=>{
+                                   return(
+                                       <Items item={item} inp={inputChangeHanlder} remove={removeHanlder} key={key}></Items>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table><br/>
+                    <label>Order
+                        <select>
+                            <option value="A">A</option> 
+                            <option value="B">B</option>
+                            <option value="C">C</option>
+                        </select>
+                    </label><br/>
+                    <button type="button" >Save</button> 
+                    <button type="button" >concel</button> 
+                </form> 
             </div>
        
         )
